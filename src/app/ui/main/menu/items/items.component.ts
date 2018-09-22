25a4e77bf1e16176dfc9f28c3b896430618e74ca
Kamule <benjamin.herbomez@gmail.com>
2018-09-22 15:58:06 +0200

    fix scss

--- src/app/ui/main/menu/items/items.component.ts
+++ src/app/ui/main/menu/items/items.component.ts
@@ -3,13 +3,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: '[app-menu-items]',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.scss']
 })
 export class ItemsComponent {
 
   @Input() signed:boolean;
   @Output() onSignout:EventEmitter<any> = new EventEmitter();
-  
+
   signout(){
       this.onSignout.emit(null);
   }
--- src/app/ui/main/menu/menu/menu.component.ts
+++ src/app/ui/main/menu/menu/menu.component.ts
@@ -5,13 +5,13 @@ declare var $:any;
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements  AfterViewInit{
 
   @Input() signed:boolean;
   @Output() onSignout:EventEmitter<any> = new EventEmitter();
-  
+
   ngAfterViewInit(){
       $('.main.menu').visibility({
           type: 'fixed',
@@ -23,11 +23,11 @@ export class MenuComponent implements  AfterViewInit{
           }
       });
   }
-  
+
   signout(){
       this.onSignout.emit(null);
   }
-  
+
   toggleSide(){
       $('.ui.sidebar').sidebar('toggle')
   }
