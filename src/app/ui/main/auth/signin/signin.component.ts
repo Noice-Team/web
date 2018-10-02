0d485a0ead3167dbf574f2df8f9339e46300cfb2
Kamule <benjamin.herbomez@gmail.com>
2018-10-02 17:48:07 +0200

    Add signin animation

--- src/app/ui/main/auth/signin/signin.component.ts
+++ src/app/ui/main/auth/signin/signin.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {TransitionController, Transition, TransitionDirection} from "ng2-semantic-ui";
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent{
+  private providerState = false;
+  public formTransitionController = new TransitionController();
+  public providerTransitionController = new TransitionController(false);
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
+  public toogleProvider() {
+    if(this.providerState){
+      this.providerTransitionController.animate(
+        new Transition('fly right', 200, TransitionDirection.Out, () => {
+          this.formTransitionController.animate(
+            new Transition('fly left', 200, TransitionDirection.In));
+        }));
+    }
+    else{
+      this.formTransitionController.animate(
+        new Transition('fly right', 200, TransitionDirection.Out, () => {
+          this.providerTransitionController.animate(
+            new Transition('fly left', 200, TransitionDirection.In));
+        }));
+    }
+    this.providerState = !this.providerState;
+  }
 }
