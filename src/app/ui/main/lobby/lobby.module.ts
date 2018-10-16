70f2f204d287d0bff6dc7c8da87bbdaaedb8524a
Kamule <benjamin.herbomez@gmail.com>
2018-10-16 22:39:02 +0200

    save

--- src/app/ui/main/lobby/lobby.module.ts
+++ src/app/ui/main/lobby/lobby.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LobbyRoutingModule } from './lobby-routing.module';
 import { MainComponent } from './main/main.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LobbyRoutingModule
   ],
-  declarations: [MainComponent, DashboardComponent]
+  declarations: [MainComponent, DashboardComponent, CreateComponent]
 })
 export class LobbyModule { }
